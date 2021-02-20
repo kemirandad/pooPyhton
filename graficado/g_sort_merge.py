346a883189d32181d4b16e22f23f7535b11006db
kemirandad <kjmd2s@gmail.com>
2021-02-20 11:07:36 -0500

    Primeros pasos con Bokeh en Python

---
+++ graficado/g_sort_merge.py
@@ -0,0 +1,82 @@
+import random
+from bokeh.plotting import figure, output_file, show
+
+contador = 0
+
+def ordenamiento_por_mezcla(lista):
+    if len(lista) > 1:
+        medio = len(lista) // 2
+        izquierda = lista[:medio]
+        derecha = lista[medio:]
+        print(izquierda, '*' * 5, derecha)
+
+        # llamada recursiva en cada mitad
+        ordenamiento_por_mezcla(izquierda)
+        ordenamiento_por_mezcla(derecha)
+
+        # Iteradores para recorrer las dos sublistas
+        i = 0
+        j = 0
+        # Iterador para la lista principal
+        k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            contar()
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+
+            k += 1
+
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k += 1
+
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+        
+        print(f'izquierda {izquierda}, derecha {derecha}')
+        print(lista)
+        print('-' * 50)
+
+    return lista
+
+def contar():
+    global contador 
+    contador += 1
+    return contador
+
+def main():
+    
+    tamano_de_lista = int(input('De que tamano sera la lista? '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
+    print(lista)
+    print('-' * 20)
+
+    lista_ordenada = ordenamiento_por_mezcla(lista)
+    print(lista_ordenada)
+    
+    output_file('g_sort_merge.html')
+    fig = figure()
+
+    x_vals = list(range(tamano_de_lista))
+    y_vals = list(range(contar()))
+    
+    for x in x_vals:
+        val = int(input(f'Valor Y para {x}: '))
+        y_vals.append(val)
+        
+    fig.line(x_vals, y_vals, line_width=2)
+    show(fig)
+    
+
+if __name__ == '__main__':
+    main()
+
